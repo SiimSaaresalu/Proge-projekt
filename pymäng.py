3b78313c8fb7e44bdd24ec428e39742afd838984
SiimSaaresalu <siim.saaresalu@ut.ee>
2021-11-10 22:12:42 +0200

    muutsin uuesti koodi

--- pymäng.py
+++ pymäng.py
@@ -2,7 +2,9 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+ekraan_laius = 800
+ekraan_pikkus = 600
+screen = pygame.display.set_mode((ekraan_laius, ekraan_pikkus))
 
 taust = pygame.image.load('oige_pilt.jpg')
 pygame.display.set_caption('Tudengi päev Tartus')
@@ -11,10 +13,12 @@ pygame.display.set_icon(icon)
  
 tegelaneImg = pygame.image.load('mängTegelane.png')
 tegelaneImg = pygame.transform.scale(tegelaneImg, (300, 540))
-tegelaneX = 250
-tegelaneY = 50
-tegelaneX_change = 0
-
+õnnelikTegelane = pygame.image.load('tegelaneÕnnelik.png')
+õnnelikTegelane = pygame.transform.scale(õnnelikTegelane, (300, 540))
+X = 250
+Y = 50
+X_change = 0
+Y_change = 0
 def player(x, y):
     screen.blit(tegelaneImg, (x, y))
     
@@ -26,32 +30,34 @@ def tekst_kõne(font : str, suurus : int, tekst : str, värv,x,y, bold : bool):
     tekstRect = tekst.get_rect()
     tekstRect.center = (x,y)
     screen.blit(tekst, tekstRect)
-    
-    
-    
+valik1 = 590, 180
+valik2 = 590, 220
+vasak_klõps = False    
     
 running = True
 while running:
     screen.fill((0, 0, 0))
     screen.blit(taust, (0, 0))
-    tekst_kõne('ARIALUNI.ttf', 30, 'Aeg AARi õppima minna', (0, 0, 0), 450, 140, False)
+    tekst_kõne('ARIALUNI.ttf', 20, 'Tere hommikust! On reede, kell on 12 ning sul on valik:', (0, 0, 0), 550, 140, False)
+    tekst_kõne('ARIALUNI.ttf', 16, 'a) kas sa lähed AARi tundi kus toimub kontrolltöö või;', (0, 0, 0), 590, 180, False)
+    tekst_kõne('ARIALUNI.ttf', 16, 'b) läheb käiku plaan Netflix ', (0, 0, 0), 590, 220, False)
     hx, hy = pygame.mouse.get_pos()
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            crashed = True
+            pygame.quit()
+            quit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                tegelaneX_change = -1
-            if event.button == 3:
-                tegelaneX_change = 1
+                vasak_klõps = True
         if event.type == pygame.MOUSEBUTTONUP:
             if event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.MOUSEBUTTONUP:
-                tegelaneX_change = 0
-    tegelaneX += tegelaneX_change         
-    if tegelaneX <=-70:
-        tegelaneX = -70
-    elif tegelaneX >= 605:
-        tegelaneX = 605
-    player(tegelaneX, tegelaneY)
+                vasak_klõps = False
+    if vasak_klõps == True:
+        if (hx, hy) == valik1:
+            ()
+        if (hx, hy) == valik2:
+            ()
+    player(X, Y)
     pygame.display.update()
