23ea15f6d7ebb60f0a1994b128a549a4993359dc
SiimSaaresalu <siim.saaresalu@ut.ee>
2021-12-14 19:21:14 +0200

    muutsin paar asja, tegin koodi ilusamaks

--- pymäng.py
+++ pymäng.py
@@ -2,18 +2,23 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+ekraan_laius = 1280
+ekraan_pikkus = 720
+screen = pygame.display.set_mode((ekraan_laius, ekraan_pikkus))
 
-taust = pygame.image.load('oige_pilt.jpg')
 pygame.display.set_caption('Tudengi päev Tartus')
 icon = pygame.image.load('images.png')
 pygame.display.set_icon(icon)
- 
-tegelaneImg = pygame.image.load('mängTegelane.png')
-tegelaneImg = pygame.transform.scale(tegelaneImg, (300, 540))
-tegelaneX = 250
-tegelaneY = 50
-tegelaneX_change = 0
+
+taustapildid = {'Korter': pygame.transform.scale(pygame.image.load('korter.jpg'), (1280, 720)),
+                'Pood': pygame.transform.scale(pygame.image.load('pood.jpg'), (1280, 720)),
+                'AAR': pygame.transform.scale(pygame.image.load('AAR.jpg'), (1280, 720)),
+                'Trepp-esik': pygame.transform.scale(pygame.image.load('trepp ees.jpg'), (1280, 720)),
+                'Trepp-põhi': pygame.transform.scale(pygame.image.load('trepp pohi.jpg'), (1280, 720)),
+                'Gen-ees': pygame.transform.scale(pygame.image.load('gen ees.jpg'), (1280, 720)),
+                'Gen-sees1': pygame.transform.scale(pygame.image.load('gen sees1.jpg'), (1280, 720)),
+                'Gen-sees2': pygame.transform.scale(pygame.image.load('gen sees2.jpg'), (1280, 720))}
+taust = taustapildid['Korter']
 
 def player(x, y):
     screen.blit(tegelaneImg, (x, y))
@@ -26,32 +31,33 @@ def tekst_kõne(font : str, suurus : int, tekst : str, värv,x,y, bold : bool):
     tekstRect = tekst.get_rect()
     tekstRect.center = (x,y)
     screen.blit(tekst, tekstRect)
-    
-    
-    
+valik1_kast = pygame.Rect(400, 170, 380, 20)
+valik2_kast = pygame.Rect(492, 210, 197, 20)
+vasak_klõps = False    
     
 running = True
 while running:
     screen.fill((0, 0, 0))
     screen.blit(taust, (0, 0))
-    tekst_kõne('ARIALUNI.ttf', 30, 'Aeg AARi õppima minna', (0, 0, 0), 450, 140, False)
-    hx, hy = pygame.mouse.get_pos()
-    
+    tekst_kõne('ARIALUNI.ttf', 20, 'Tere hommikust! On reede, kell on 12 ning sul on valik:', (0, 0, 0), 550, 140, False)
+    tekst_kõne('ARIALUNI.ttf', 16, 'a) kas sa lähed AARi tundi kus toimub kontrolltöö või;', (0, 0, 0), 590, 180, False)
+    tekst_kõne('ARIALUNI.ttf', 16, 'b) läheb käiku plaan Netflix ', (0, 0, 0), 590, 220, False)
+    mpos = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            crashed = True
+            pygame.quit()
+            quit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                tegelaneX_change = -1
-            if event.button == 3:
-                tegelaneX_change = 1
+                vasak_klõps = True
         if event.type == pygame.MOUSEBUTTONUP:
             if event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.MOUSEBUTTONUP:
-                tegelaneX_change = 0
-    tegelaneX += tegelaneX_change         
-    if tegelaneX <=-70:
-        tegelaneX = -70
-    elif tegelaneX >= 605:
-        tegelaneX = 605
-    player(tegelaneX, tegelaneY)
+                vasak_klõps = False
+    if vasak_klõps == True:
+        if valik1_kast.collidepoint(mpos):
+            if taust == taustapildid['Korter']:
+                taust = taustapildid['Pood']
+        if valik2_kast.collidepoint(mpos):
+            print('Tubli!')
     pygame.display.update()
